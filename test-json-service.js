44a8f05ece1b16aceee182e8af3aa4d8f2a2453c
Mark Saunders <enfortis.tech@gmail.com>
2020-10-26 08:24:56 -0500

    CODE: added remaining code

--- test-json-service.js
+++ test-json-service.js
@@ -3,7 +3,26 @@ const server = require('net').createServer(connection => {
     console.log('Subscriber connected.');
 
     // two message chunks that together make a whole message
-    const firstChunk = '{"type": "changed", "timesta'
-    const secondChunk = 'mp": 145065945987}'
-})
+    const firstChunk = '{"type": "changed", "timesta';
+    const secondChunk = 'mp": 145065945987}\n';
+
+    // send the first chunk immediately
+    connection.write(firstChunk);
+
+    //after a short delay send the second chunk
+    const timer = setTimeout(() => {
+        connection.write(secondChunk);
+        connection.end();
+    },100);
+
+    //clear teimer when the connection ends
+    connection.on('end', () => {
+        clearTimeout(timer);
+        console.log('Subscriber disconnected');
+    });
+});
+
+server.listen(60300, function() {
+    console.log('Test server listening for subscribers...');
+});
 
